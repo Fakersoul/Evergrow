fdc2058348d93ad38f8095c73336f2d62949973f
Sebastian Lindgren <sebustator@gmail.com>
2020-11-06 15:31:49 +0200

    Particles separated, Collision, StormCloudGenerator, New Follow
    Particles will not follow emitters no more, Collision for particles added so when colliding they will cause wanted effect, Created better version of generator for stormclouds, generators follow as trees grow.

---
+++ Assets/ParticleCollisionTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollisionTest : MonoBehaviour
+{
+
+    private void OnParticleCollision(GameObject other)
+    {
+       // Debug.Log("Colliding");// Add wanted function here...
+    }
+
+}
---
+++ Assets/StormCloudGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StormCloudGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] stormClouds;
+
+    [SerializeField]
+    float spawnrate;
+
+    [SerializeField]
+    GameObject stormCloudEndpoint;
+
+    Vector3 stormStartPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stormStartPosition = transform.position;
+
+        Invoke("AttemptStormSpawn", spawnrate); //if too slow, try coroutine!
+        
+    }
+
+    void SpawnStormCloud()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, stormClouds.Length);
+        GameObject StormCloud = Instantiate(stormClouds[randomIndex]);
+
+        StormCloud.transform.position = stormStartPosition;
+    }
+
+    void AttemptStormSpawn()
+    {
+        SpawnStormCloud();
+
+        Invoke("AttemptStormSpawn", spawnrate);
+
+    }
+
+
+}
